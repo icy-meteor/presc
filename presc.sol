eb647434e42e729a0aa4fbbab4e3a086e7e7d09f
Rahul Suresh <rahul.tech.work@gmail.com>
2018-10-12 14:38:29 +0530

    Update presc.sol

---
+++ presc.sol
@@ -0,0 +1,81 @@
+
+Nilesh Matai
+2:36 PM (1 minute ago)
+to Rahul
+
+pragma solidity ^0.4.20;
+
+contract doctor{
+    mapping (address=>bool) approvers;
+    struct doctorStruct{
+        address add;
+        string name;
+        string hospital;
+        string speciality;
+    }
+    doctorStruct[] public doctors;
+    function getNewDoctor(address add,string name,string hospital,string speciality) public
+    {
+        doctorStruct memory newDoc=doctorStruct({
+            add:add,
+            name:name,
+            hospital:hospital,
+            speciality:speciality
+        });
+        approvers[add]=true;
+        doctors.push(newDoc);
+    }
+    function  checkIfdoctor(address checkDoc) view external returns(bool)
+    {
+        return approvers[checkDoc];
+    }
+  }
+  
+contract patient{
+    mapping (address=>bool) approvers;
+    struct patientStruct{
+        address add;
+        string name;
+        uint age;
+        uint weight;
+    }
+    patientStruct[] public patients;
+    function getNewPatient(address add,string name,uint age,uint weight) public
+    {
+        patientStruct memory newPat=patientStruct({
+            add:add,
+            name:name,
+            age:age,
+            weight:weight
+        });
+        approvers[add]=true;
+        patients.push(newPat);
+    }
+    function  checkIfpatient(address checkPat) view external returns(bool)
+    {
+        return approvers[checkPat];
+    }
+  }
+  contract pharmacy{
+    mapping (address=>bool) approvers;
+    struct pharmacyStruct{
+        address add;
+        string name;
+        uint licenseNo;
+    }
+    pharmacyStruct[] public pharmacys;
+    function getNewPharmacy(address add,string name,uint licenseNo) public
+    {
+        pharmacyStruct memory newPhar=pharmacyStruct({
+            add:add,
+            name:name,
+            licenseNo:licenseNo
+        });
+        approvers[add]=true;
+        pharmacys.push(newPhar);
+    }
+    function  checkIfpharmacy(address checkPhar) view external returns(bool)
+    {
+        return approvers[checkPhar];
+    }
+  }
